7d329afde118d7c4f560b1098f855e8215335ae0
tin2tin <tintwotin@gmail.com>
2021-06-11 06:10:12 +0200

    Add more checks

--- __init__.py
+++ __init__.py
@@ -25,21 +25,32 @@ class SEQUENCER_OT_match_frame(bpy.types.Operator):
         current_scene = bpy.context.scene
         current_frame = current_scene.frame_current
         active = current_scene.sequence_editor.active_strip
-        find_frame = current_frame - active.frame_final_start
-        print(find_frame)
-        type = active.type
-  
-            
+
+        frame_start = (active.frame_start + active.frame_offset_start)
+        frame_end = (active.frame_start + active.frame_offset_start + active.frame_final_duration)
+
+        if current_frame >=frame_start and current_frame <= frame_end:
+            find_frame = current_frame - active.frame_start
+        else:
+            find_frame = 0
+
         for sce in bpy.data.scenes:
             seq = sce.sequence_editor
 
             for strip in seq.sequences_all:
-                if current_scene.name != sce.name and type == strip.type and (active.name == strip.name or active.name[:-4].find(strip.name) >= 0):
-                    win = bpy.context.window_manager.windows[0]
-                    win.scene = bpy.data.scenes[sce.name]
-                    bpy.context.scene.frame_current = (find_frame + strip.frame_final_start)
-                    bpy.ops.sequencer.view_frame()
-                    break
+                if find_frame and current_scene.name != sce.name and active.type == strip.type and (active.name == strip.name or active.name[:-4].find(strip.name)):
+                    frame_current = (find_frame + strip.frame_start)
+                    frame_start = (strip.frame_start + strip.frame_offset_start)
+                    frame_end = (strip.frame_start + strip.frame_offset_start + strip.frame_final_duration)
+
+                    if frame_current >= frame_start and frame_current <= frame_end:
+                        win = bpy.context.window_manager.windows[0]
+                        win.scene = bpy.data.scenes[sce.name]
+                        bpy.context.scene.frame_current = frame_current
+                        bpy.context.scene.sequence_editor.active_strip = strip
+                        bpy.ops.sequencer.view_frame()
+                        break
+                        break
         return {"FINISHED"}
 
 
